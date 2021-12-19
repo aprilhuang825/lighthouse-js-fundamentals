f1332392b0602d9b08c9b9e6df2dda2a71c9fdd8
Ziyu Huang <aprilhuang825@gmail.com>
2021-12-19 19:57:23 +0000

    New changes

--- hello.js
+++ hello.js
@@ -5,3 +5,14 @@ const sayHello = function(name){
 sayHello("Caliban");
 sayHello("Miranda");
 sayHello("Ferdinand");
+
+const sayHelloToConsole = function (name){
+  console.log("Hello, " + name);
+}
+sayHelloToConsole("John");
+
+const returnSayHello = function (name){
+  return "Hello, " + name;
+}
+const greting = returnSayHell("John");
+console.log(greting);
